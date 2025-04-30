73953077f8e7b2ca89c83e7940b2ae46be637ea5
davariijs <atuor127@gmail.com>
2025-05-01 01:27:52 +0330

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { Layout, Typography } from 'antd';
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: '#fff',
+          borderBottom: '1px solid #f0f0f0',
+        }}
+      >
+        <Title level={3} style={{ margin: 0 }}>
+          Domains Management
+        </Title>
+      </Header>
+      <Content style={{ padding: '20px 50px' }}>
+        <p>Welcome to the Domain Management App!</p>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Front-End Assessment ©{new Date().getFullYear()}
+      </Footer>
+    </Layout>
   );
 }
 
